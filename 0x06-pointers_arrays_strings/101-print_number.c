b0f70218303222a6c9ce91a7fc2c6bb9daff7b56
AnthemPython || Related <111581268+SeasonLeague@users.noreply.github.com>
2022-09-21 20:01:30 +0100

    Update file 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,45 @@
-#include "main.h"
+#include "holberton.h"
 
+/**
+ * print_number - prints an integer
+ *
+ * @num: int to print
+ *
+ * Return: void
+ */
 
-
-
-void print_number(int n)
+void print_number(int num)
 {
-unsigned int m, d, count;
+	unsigned int d = 10, n;
 
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
-
-d = m;
-count = 1;
-
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
