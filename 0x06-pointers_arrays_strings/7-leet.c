fcf1f7c42e1bc8c5ba9df580acadb88bc1b0de07
AnthemPython || Related <111581268+SeasonLeague@users.noreply.github.com>
2022-09-21 19:55:22 +0100

    Update file 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,25 @@
 #include "main.h"
-char *leet(char *str)
+#include <stdio.h>
+/**
+* *leet - encodes a string into 1337
+*@c: string received
+*Return: string changed
+*/
+char *leet(char *c)
 {
-	int i = 0, k;
-	char s[] = "aAeEoOtTlL";
-	char s1[] = "4433007711";
+int i, j;
+char a[] = "oOlLeEaAtT";
+char n[] = "0011334477";
 
-	for (; str[i] != '\0'; i++)
-	{
-		for (k = 0; k <= 9; k++)
-		{
-			if (s[k] == str[i])
-			{
-				str[i] = s1[k];
-			}
-		}
-	}
-	return (str);
+for (i = 0; c[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+if (c[i] == a[j])
+{
+c[i] = n[j];
+}
+}
+}
+return (c);
 }
