0b249e8f15d5fe789954456d0f78edee87a38131
SeasonLeague <facebookups23@gmail.com>
2022-09-30 07:30:59 -0700

    THis commit Fixed typo launcher Thank you Alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints program name, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)argc;
-printf("%s\n", argv[0]);
-return (0);
+	printf("%s\n", *argv);
+
+	return (0);
 }
