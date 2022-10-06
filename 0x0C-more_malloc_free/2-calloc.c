d9ec05de59c2f5fcb2445abab5ba23a0cfd2b0b7
SeasonLeague <facebookups23@gmail.com>
2022-10-06 06:40:14 -0700

    Corrected a typo in 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,43 +1,33 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-char *ptr = s;
-
-while (n--)
-	*s++ = b;
-
-return (ptr);
-}
-
-/**
- * *_calloc - allocates memory for an array, using malloc
- * @nmemb: array length
- * @size: size of each element
+ * _calloc - allocates memory for an array
+ * @nmemb: first input parameter
+ * @size: second input parameter
+ *
  * Return: pointer
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *m;
+	char *pointer;
+	unsigned int i;
 
-if (size == 0 || nmemb == 0)
-	return (NULL);
-
-m = malloc(nmemb * size + 1);
-
-if (m == 0)
-	return (NULL);
-
-_memset(m, 0, nmemb * size);
-
-return (m);
+	if (!nmemb || !size)
+	{
+		return (NULL);
+	}
+	pointer = malloc(nmemb * size);
+	if (!pointer)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*(pointer + i) = 0;
+	}
+	return (pointer);
 }
