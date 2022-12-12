0acbdbdb8711ae2e862289a28278f33253253ff8
SeaonLeague <facebookups23@gmail.com>
2022-12-12 09:37:18 -0800

    I thinking...

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/nyangoto/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=/tmp/libwinner.so
+cp ../sh2.sh .
+. ./sh2.sh
